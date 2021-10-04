12c13b24941ea5641b6c690852925ca2cc5e2b20
Sam Carbine <scarbine@me.com>
2021-10-04 15:20:38 -0500

    adding coffee works

--- js-app/main.js
+++ js-app/main.js
@@ -1,6 +1,6 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
-const coffeeUrl = "https://localhost:5001/api/Coffee/"
+const coffeeUrl = "https://localhost:5001/api/coffee/"
 
 let varietiesString = "<h3> Hello, It's me. Coffee Variety. </h3>";
 let coffeeString = "<h3> Hello, It's me. Coffee. </h3>"
@@ -8,11 +8,18 @@ let coffeeString = "<h3> Hello, It's me. Coffee. </h3>"
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
-    .then(getAllCoffee())
+    .then(getAllCoffee)
 
 });
 
-function getAllBeanVarieties() {
+const coffeeButton = document.querySelector("#add-coffee");
+coffeeButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    addCoffee()
+    
+})
+
+const getAllBeanVarieties = () => {
     return fetch(url).then(res => res.json())
     .then(beanVarieties => {
         beanVarieties.map((v) => {
@@ -20,7 +27,7 @@ function getAllBeanVarieties() {
     })}).then(displayBeans);
 }
 
-function getAllCoffee(){
+const getAllCoffee = () =>{
     return fetch(coffeeUrl).then(res => res.json())
             .then(coffee => {
             coffee.map((c) => {
@@ -28,6 +35,21 @@ function getAllCoffee(){
             })
          }) .then(displayCoffee)
 }
+
+const addCoffee = () =>{
+    const coffeeObj = {
+        title: document.getElementById("coffeeTitle").value,
+        beanVarietyId : document.getElementById("beanVarietyId").value
+    }
+    return fetch(coffeeUrl,{
+		method: "POST",
+		headers:{
+			"Content-Type": "application/json"
+		},
+		body:JSON.stringify(coffeeObj)})
+}
+
+
 
 const displayBeans = () => {
     document.getElementById("types").innerHTML = varietiesString;
