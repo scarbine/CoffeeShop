20be3f333fa5293797da79886bab3aab15abd648
Sam Carbine <scarbine@me.com>
2021-10-04 12:03:38 -0500

    added repo, controller, and Models for coffee

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public int BeanVarietyId { get; set; }
+    }
+}
